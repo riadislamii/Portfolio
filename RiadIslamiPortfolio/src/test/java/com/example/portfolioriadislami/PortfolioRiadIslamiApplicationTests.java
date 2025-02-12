1cb06cacaad74db96ec1dcd9627af76fc54b3f6c
riadislamii <riad.islami@student.cacttus.education>
2025-02-12 16:34:40 +0100

    Add files via upload

---
+++ RiadIslamiPortfolio/src/test/java/com/example/portfolioriadislami/PortfolioRiadIslamiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.portfolioriadislami;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioShpatKonjushaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
